92b03780306ad1d339174997f1a34d297389a19e
deekshakumarics25-boop <my9934996@gmail.com>
2026-05-05 15:37:36 +0530

    Add latest controller and utils files

---
+++ utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+exports.generateAccessToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "15m" });
+};
+
+exports.generateRefreshToken = (id) => {
+  return jwt.sign({ id }, process.env.REFRESH_SECRET, { expiresIn: "7d" });
+};
